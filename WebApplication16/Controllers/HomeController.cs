3b3bed6fd5266d790a07c591dd9ac42f893781a5
Avrumi Friedman <alex+6@lakewoodprogramming.com>
2018-03-27 22:08:10 -0400

    Initial commit

---
+++ WebApplication16/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication16.Models;
+
+namespace WebApplication16.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            CarTwistDb db = new CarTwistDb(Properties.Settings.Default.ConStr);
+            IEnumerable<Car> cars = db.GetCars();
+            return View(cars);
+        }
+
+        public ActionResult ShowAddForm()
+        {
+            return View();
+        }
+
+
+        public ActionResult AddCar(Car car)
+        {
+            CarTwistDb db = new CarTwistDb(Properties.Settings.Default.ConStr);
+            db.AddCar(car);
+            return Redirect("/");
+        }
+    }
+}
---
+++ WebApplication16/Scripts/add-car.js
@@ -0,0 +1,36 @@
+﻿$(() => {
+
+    $("input.form-control").on('keyup', function () {
+        ensureFormValidity();
+    });
+
+    $("#car-type").on('change', function () {
+        ensureFormValidity();
+        const carType = $("#car-type").val();
+        $("#has-leather-seats").prop('checked', carType === "2");
+        $("#has-leather-seats").prop('disabled', carType === "2");
+        if (carType === "2") {
+            $("form").append("<input type='hidden' name='hasLeatherSeats' id='hidden-seats' value='true' />");
+        } else {
+            $("#hidden-seats").remove();
+        }
+    });
+
+    function ensureFormValidity() {
+        const isValid = isFormValid();
+        $("#submit-button").prop('disabled', !isValid);
+    }
+
+    function isFormValid() {
+        const make = $("#make").val();
+        const model = $("#model").val();
+        const year = $("#year").val();
+        const price = $("#price").val();
+        const carType = $("#car-type").val();
+        return make && model && year && price && carType;
+    }
+
+    //$("form").on('submit', function () {
+    //    $("#has-leather-seats").prop('disabled', false);
+    //});
+});
